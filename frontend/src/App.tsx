f91dc9c6c0f22f38465a78391fd856dd0d09bfe7
BenjaminIndo <benjamin.indo@ug.uchile.cl>
2025-09-17 20:19:39 -0300

    feat: add details on click

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,43 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 import GameDisplay from "../src/components/gameDisplay";
-import { mockGames } from "../../backend/data/mockGames"
-import type { Game } from "../src/model/game";
+import { mockGames } from "../../backend/data/mockGames";
+// import type { Game } from "../src/model/game";
 
 function App() {
-  const [games, setGames] = useState<Game[]>(mockGames)
-
-  setGames
+  // const [games, setGames] = useState<Game[]>(mockGames);
+  const [selectedGameIndex, setSelectedGameIndex] = useState<number | null>(
+    null
+  );
+  const selectedGame =
+    selectedGameIndex !== null ? mockGames[selectedGameIndex] : null;
+  // setGames;
 
   return (
     <>
       <div>
-          <h1>GameBoxd</h1>
-          {games.map(e => <GameDisplay game={e}/>)}
+        <h1>GameBoxd</h1>
+        {!selectedGame ? (
+          <div>
+            {mockGames.map((game, index) => (
+              <GameDisplay
+                key={index}
+                game={game}
+                onClick={() => setSelectedGameIndex(index)}
+              />
+            ))}
+          </div>
+        ) : (
+          <div>
+            <GameDisplay game={selectedGame} detailed />
+            <button onClick={() => setSelectedGameIndex(null)}>
+              Volver a la lista
+            </button>
+          </div>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/gameDisplay.tsx
+++ frontend/src/components/gameDisplay.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import { useState } from "react";
 import type { Game } from "../model/game";
 
-interface GameDisplayProps{
-    game: Game
+interface GameDisplayProps {
+  game: Game;
+  detailed?: boolean;
+  onClick?: () => void;
 }
 
-const GameDisplay=({game}: GameDisplayProps) => {
-    
-    return (
-        <div className="card">
-            <p>Nombre: {game.name}</p>
-            <p>Autor: {game.creator}</p>
-            <p>Año de lanzamiento: {game.release_year}</p>
-            <p>Género/s: {game.genre.map(e => e + ", ")}</p>
-        </div>
-    )
-}
+const GameDisplay = ({ game, detailed = false, onClick }: GameDisplayProps) => {
+  return (
+    <div className="card" onClick={onClick}>
+      <p>Nombre: {game.name}</p>
+      <p>Autor: {game.creator}</p>
+      {detailed && (
+        <>
+          <p>Año de lanzamiento: {game.release_year}</p>
+          <p>Género/s: {game.genre.map((e) => e + ", ")}</p>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default GameDisplay;
