f6a4dd09a3c73be3211e26b854d1f1994ea66b8a
pagliacci03 <rdiaz6117@gmail.com>
2025-10-18 00:25:48 -0300

    chore: configure proxy to connect to backend

--- frontend/src/services/login.ts
+++ frontend/src/services/login.ts
@@ -20,7 +20,7 @@ const login = async (credentials: Credentials) => {
 
 const restoreLogin = async () => {
     try {
-        const response = await axiosSecure.get("/api/login/me");
+        const response = await axiosSecure.get("/api/login/auth/me");
         return response.data; // Usuario logueado
     } catch {
         return null; // No logueado
--- frontend/src/services/users.ts
+++ frontend/src/services/users.ts
@@ -1,10 +1,5 @@
 import axiosSecure from "../utils/axiosSecure";
 
-const getCurrentUser = async () => {
-    const response = await axiosSecure.get("/api/users/me");
-    return response.data;
-}
-
 const addToFavorites = async (gameId: string) => {
     const response = await axiosSecure.post("/api/users/favorites", { gameId });
     return response.data;
@@ -25,4 +20,4 @@ const removeFromWishlist = async (gameId: string) => {
     return response.data;
 }
 
-export default { getCurrentUser, addToFavorites, addToWishlist, removeFromFavorites, removeFromWishlist }
+export default { addToFavorites, addToWishlist, removeFromFavorites, removeFromWishlist }
